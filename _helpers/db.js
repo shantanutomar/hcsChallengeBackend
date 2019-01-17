5d8b510143bf82d5e2aa4dd4d1c30654a8fc522d
shantanutomar <shaan046@gmail.com>
2019-01-17 23:34:51 +0530

    commit

--- _helpers/db.js
+++ _helpers/db.js
@@ -1,8 +1,21 @@
-const config = require('config.json');
-const mongoose = require('mongoose');
-mongoose.connect(process.env.MONGODB_URI || config.connectionString);
+const config = require("config.json");
+const mongoose = require("mongoose");
+// mongoose.connect(process.env.MONGODB_URI || config.connectionString);
+mongoose.connect(
+  config.connectionString,
+  function(err) {
+    // {
+    if (err) {
+      console.log("Some problem with the connection " + err);
+    } else {
+      console.log("The Mongoose connection is ready");
+    }
+  }
+);
+
+// mongoose.connect(config.connectionString);
 mongoose.Promise = global.Promise;
 
 module.exports = {
-    User: require('../users/user.model')
+  User: require("../users/user.model")
 };
